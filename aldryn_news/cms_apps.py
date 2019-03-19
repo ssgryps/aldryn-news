76a1049b0f93ac0be5f06142eec6caf67a30bcc2
Dmytro Litvinov <litvinov.dmytro.it@gmail.com>
2019-03-19 18:29:39 +0200

    Fix import

--- aldryn_news/cms_apps.py
+++ aldryn_news/cms_apps.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.utils.translation import ugettext_lazy as _
 
-from aldryn_news.menu import NewsCategoryMenu
+from aldryn_news.cms_menus import NewsCategoryMenu
 
 from cms.app_base import CMSApp
 from cms.apphook_pool import apphook_pool
